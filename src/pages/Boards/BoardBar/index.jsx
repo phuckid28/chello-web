52d5c12bdaea0831bb6b8f4182fe5c68ab9b9522
phuckid28 <113428984+phuckid28@users.noreply.github.com>
2025-05-17 16:55:20 +0700

    board-bar-v1

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,121 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterAltIcon from '@mui/icons-material/FilterAlt'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Tooltip } from '@mui/material'
+import Button from '@mui/material/Button'
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="Phuckid28"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private workspaces"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterAltIcon />}
+          label="Filter"
+          clickable
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant='outlined'startIcon={<PersonAddAltIcon />}>Invite</Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: '34px',
+              height: '34px',
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='Phuckid28'>
+            <Avatar
+              alt="Phuckid28"
+              src='src\assets\sora.jpg'
+            />
+          </Tooltip>
+          <Tooltip title='Phuckid28'>
+            <Avatar
+              alt="Phuckid28"
+              src='src\assets\sora.jpg'
+            />
+          </Tooltip>
+          <Tooltip title='Phuckid28'>
+            <Avatar
+              alt="Phuckid28"
+              src='src\assets\sora.jpg'
+            />
+          </Tooltip>
+          <Tooltip title='Phuckid28'>
+            <Avatar
+              alt="Phuckid28"
+              src='src\assets\sora.jpg'
+            />
+          </Tooltip>
+          <Tooltip title='Phuckid28'>
+            <Avatar
+              alt="Phuckid28"
+              src='src\assets\sora.jpg'
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+
     </Box>
   )
 }
