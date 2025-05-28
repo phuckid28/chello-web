68fe558ad0ca6433dec521d78634efb6468e4861
phuckid28 <113428984+phuckid28@users.noreply.github.com>
2025-05-29 00:08:50 +0700

    Update _id.jsx
    Fix useEffect

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,7 +3,6 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
 
@@ -17,7 +16,7 @@ function Board() {
     fetchBoardDetailsAPI(boardId).then(board => {
       setBoard(board)
     })
-  })
+  }, [])
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
