a70e24bb3c038da184787830b3bb39bf5fdaa999
phuckid28 <113428984+phuckid28@users.noreply.github.com>
2025-06-07 00:12:57 +0700

    routerDom-and-hookForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Board from '~/pages/Boards/_id'
+import NotFound from './pages/404/NotFound'
+import Auth from './pages/Auth/Auth'
 
 function App() {
   return (
-    <>
-      {/* React Router Dom  */}
+    <Routes>
+      {/** Redirect Route */}
+      <Route path='/' element={
+        // Replace giá trị true để nó thay thế route / (route / sẽ không còn nằm trong history của Browser)
+        // Test bằng cách chọn Go Home từ trang 404 xong quay lại bằng nút back của trình duyệt giữa 2 trường hợp có replace hoặc không có
+        <Navigate to="/boards/683802ed26942f2c2cc289fa" replace={true}/>
+      } />
+
       {/** Board Deatils */}
-      <Board />
-    </>
+      <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authentication */}
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
+
+      {/* 404 not found page */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
